b4242141ae3d89f2be4c07d3b654ad15853fde8f
LuthfiSyarifHidayatullah <ufisyarif@gmail.com>
2022-04-25 15:35:51 +0700

    Shayna

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <header-shayna />
+   <hero-section />
+   <women-shayna/>
+   <insta-shayna/>
+   <partner-shayna/>
+   <footer-shayna/>
+    
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroSection ,
+    WomenShayna , 
+    InstaShayna ,
+    PartnerShayna,
+    FooterShayna
+    
+  }
+}
+</script>
